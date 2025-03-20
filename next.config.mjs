411b455b0b44759a3542a34fd31804b0dedbf7ba
deeakpan <deeakpan123@gmail.com>
2025-03-20 17:53:28 +0100

    Fix Netlify deployment with static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export", // Enables static export (Fixes "next export" issue)
   images: {
+    unoptimized: true, // Fixes image optimization issue in static exports
     remotePatterns: [
       {
         protocol: "https",
