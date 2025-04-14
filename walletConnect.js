50a7f41f0cba150effa87a27a90d7b314afc2b54
deeakpan <deeakpan123@gmail.com>
2025-04-14 13:28:56 +0100

    fixed rainbowkit mobile display

--- walletConnect.js
+++ walletConnect.js
@@ -1,7 +1,9 @@
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { metaMaskWallet } from '@rainbow-me/rainbowkit/wallets';
-import { http, createConfig } from 'wagmi';
-
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  RainbowKitProvider,
+  getDefaultConfig,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
 
 // Define the Pepe Unchained chain
 export const pepeUnchained = {
@@ -17,28 +19,40 @@ export const pepeUnchained = {
     default: { http: ['https://3409.rpc.thirdweb.com'] },
   },
   blockExplorers: {
-    default: { 
-      name: 'Pepe Unchained Explorer', 
-      url: 'https://explorer-pepe-unchained-gupg0lo9wf.t.conduit.xyz/' 
+    default: {
+      name: 'Pepe Unchained Explorer',
+      url: 'https://explorer-pepe-unchained-gupg0lo9wf.t.conduit.xyz/',
     },
   },
+  iconUrl: 'https://raw.githubusercontent.com/base-org/brand-kit/001c0e9b40a67799ebe0418671ac4e02a0c683ce/logo/in-use/icon/base-logo.svg',
+  iconBackground: '#000000',
 };
 
-// Configure project ID
+// Project ID from WalletConnect
 const projectId = 'd0b2dab20e3667281d013129f7f38720';
 
-// Create wagmi config with the Pepe chain
-export const config = createConfig({
+// Full wallet config (not limited to MetaMask)
+export const config = getDefaultConfig({
+  appName: 'Pepe Unchained',
+  projectId,
   chains: [pepeUnchained],
-  transports: {
-    [pepeUnchained.id]: http('https://3409.rpc.thirdweb.com'),
-  },
+  ssr: true,
+  initialChain: pepeUnchained,
 });
 
-// Set up wallets (only MetaMask)
-export const wallets = [
-  metaMaskWallet({ projectId }),
-];
+// App-wide provider setup
+export const WalletProvider = ({ children }) => {
+  return (
+    <WagmiProvider config={config}>
+      <RainbowKitProvider
+        showRecentTransactions={true}
+        chains={[pepeUnchained]}
+      >
+        {children}
+      </RainbowKitProvider>
+    </WagmiProvider>
+  );
+};
 
 // Function to add Pepe Unchained network to MetaMask
 export const addPepeUnchainedToMetaMask = async () => {
@@ -71,7 +85,6 @@ export const switchToPepeUnchained = async () => {
       });
       console.log('Switched to Pepe Unchained network');
     } catch (error) {
-      // This error code indicates that the chain has not been added to MetaMask
       if (error.code === 4902) {
         await addPepeUnchainedToMetaMask();
       } else {
